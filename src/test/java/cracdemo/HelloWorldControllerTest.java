5a8c511b7a7d9a618e1175b5407ea7d525063859
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-22 07:38:14 +0100

    split tests

--- src/test/java/cracdemo/HelloWorldControllerTest.java
+++ src/test/java/cracdemo/HelloWorldControllerTest.java
@@ -24,47 +24,16 @@ class HelloWorldControllerTest {
 
     @Test
     void emulateCheckpoint() {
-        try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class, Environment.TEST)) {
-            CheckpointSimulator checkpointSimulator =
-                    server.getApplicationContext().getBean(CheckpointSimulator.class);
-            testApp(server, this::testHelloWorld);
-
-            Object time = testApp(server, this::testTime);
-            assertEquals(time, testApp(server, this::testTime));
-
-            checkpointSimulator.runBeforeCheckpoint();
-            server.stop();
-            checkpointSimulator.runAfterRestore();
-            server.start();
-            testApp(server, this::testHelloWorld);
-            assertNotEquals(time, testApp(server, this::testTime));
-        }
-    }
-
-    private Object testApp(EmbeddedServer embeddedServer, Function<BlockingHttpClient, Object> clientConsumer) {
-        try (HttpClient httpClient = embeddedServer.getApplicationContext().createBean(HttpClient.class, embeddedServer.getURL())) {
-            BlockingHttpClient client = httpClient.toBlocking();
-            return clientConsumer.apply(client);
-        }
+        CheckpointTestUtils.test(this::testHelloWorld);
     }
 
     private Void testHelloWorld(BlockingHttpClient client) {
-        HttpResponse<Map<String, String>> response = client.exchange(HttpRequest.GET("/"), Argument.mapOf(String.class, String.class));
+        HttpResponse<Map<String, String>> response = client.exchange(HttpRequest.GET("/"),
+                Argument.mapOf(String.class, String.class));
         assertEquals(HttpStatus.OK, response.getStatus());
         Optional<Map<String, String>> bodyOptional = response.getBody();
         assertTrue(bodyOptional.isPresent());
         assertEquals(Collections.singletonMap("message", "Hello World"), bodyOptional.get());
         return null;
-    }
-
-    private String testTime(BlockingHttpClient client) {
-        HttpResponse<Map<String, String>> response = client.exchange(HttpRequest.GET("/time"), Argument.mapOf(String.class, String.class));
-        assertEquals(HttpStatus.OK, response.getStatus());
-        Optional<Map<String, String>> bodyOptional = response.getBody();
-        assertTrue(bodyOptional.isPresent());
-        Map<String, String> body = bodyOptional.get();
-        assertEquals(1, body.keySet().size() );
-        assertTrue(body.containsKey("time"));
-        return body.get("time");
     }
 }
