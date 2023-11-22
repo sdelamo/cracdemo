9bd16549ef6068777f42300632d8f446850a5bbc
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-22 06:10:09 +0100

    test eagerly initialized

---
+++ src/test/java/cracdemo/Clock.java
@@ -0,0 +1,18 @@
+package cracdemo;
+
+import jakarta.inject.Singleton;
+
+import java.time.LocalDateTime;
+
+@Singleton
+class Clock {
+    private final LocalDateTime now;
+
+    Clock() {
+        now = LocalDateTime.now();
+    }
+
+    LocalDateTime getNow() {
+        return now;
+    }
+}
